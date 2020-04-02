6aeada7a76f400481d50e5b6449003af41ca0295
kentxxq <admin@kentxxq.com>
2020-04-02 23:17:57 +0800

    准备用kaitai来解决，留个坑吧

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
-from VideoInfo import Mp4Url
+# from VideoInfo import Mp4Url
 
-mp4 = Mp4Url('http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4')
-mp4.getAll()
-print(mp4)
+# mp4 = Mp4Url('http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4')
+# mp4.getAll()
+# print(mp4)
 
+from temp.jpeg import Jpeg
+
+j = Jpeg.from_file(
+    r"C:\\Users\\kentxxq\Desktop\\a01de1df06bbc2563316d5e579cb9b79_1200x500.jpg"
+)
+
+print(j.segments)
